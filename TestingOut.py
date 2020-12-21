4be6dd4d1ed966e0de5784c452eff136a35bebc4
minh-p <minhisverynormal@gmail.com>
2020-12-21 13:58:21 -0800

    Got through chapter 3, gonna learn chapter 4 later.

--- TestingOut.py
+++ TestingOut.py
@@ -14,7 +14,18 @@ def get_the_sum_of_numbers(*numbers):
     return current_sum_of_number
 
 def return_something():
-    my_string = "This is my lovely string."
-    return my_string[0:-5]
+    random_list = [1, "string", 2, None]
+
+    return random_list
 
 print(return_something())
+
+def do_fibonacci_sequence():
+    a = 0
+    b = 1
+
+    while a < 100:
+        print(a, end=", ")
+        b, a = a+b, b
+
+do_fibonacci_sequence()
